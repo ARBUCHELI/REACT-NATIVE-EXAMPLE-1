93dc6ca2c9f42ec9164d644dcc9c01bcbeffa4bb
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-09-14 10:49:55 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Text>My name is Andres!</Text>
+      <Text>This is a new React Native App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#92a8d1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
